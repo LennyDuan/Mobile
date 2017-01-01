1d3091193147d26e8dfdb940d375150a4753324a
Lenny Duan <dhy939694@gmail.com>
2017-01-01 10:08:25 +0000

    image picker

--- IOS/TODO/TODO/ToDo/ImageEditViewController.swift
+++ IOS/TODO/TODO/ToDo/ImageEditViewController.swift
@@ -8,26 +8,86 @@
 
 import UIKit
 
-class ImageEditViewController: CommonViewController {
+class ImageEditViewController: CommonViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        super.hideKeyboardWhenTappedArround()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    
+    @IBAction func saveTap(_ sender: Any) {
+        saveImgToLibrary()
     }
-    */
+    @IBOutlet weak var imageView: UIImageView!
+    
+    
+    
+    
+    // Select Image or Take a photo
+    @IBAction func selectImageTap(_ sender: Any) {
+        
+        let alert = UIAlertController(title: "Related Image", message: "Take a photo or choose image from album library", preferredStyle: UIAlertControllerStyle.actionSheet)
+        
+        let actionAlbum = UIAlertAction(title: "Select From Album", style: UIAlertActionStyle.default, handler: selectAlbum)
+        
+        let actionCamera = UIAlertAction(title: "Use cemara", style: UIAlertActionStyle.default, handler: takeCamera)
+        
+        let actionCancel = UIAlertAction(title: "Cancel", style: UIAlertActionStyle.default, handler: nil)
+        self.present(alert, animated: true, completion: nil)
+        alert.addAction(actionAlbum)
+        alert.addAction(actionCamera)
+        alert.addAction(actionCancel)
 
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        if let image  = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            imageView.image = image
+            imageView.contentMode = .scaleAspectFit
+            self.dismiss(animated: true, completion: nil)
+        }
+        
+    }
+    func selectAlbum(action: UIAlertAction) {
+        if UIImagePickerController.isSourceTypeAvailable(
+            UIImagePickerControllerSourceType.photoLibrary)
+        {
+            let imagePicker = UIImagePickerController()
+            imagePicker.delegate = self
+            imagePicker.sourceType = UIImagePickerControllerSourceType.photoLibrary
+            imagePicker.allowsEditing = true
+            self.present(imagePicker, animated: true, completion: nil)
+        }
+    }
+    
+    func takeCamera(action: UIAlertAction) {
+        if UIImagePickerController.isSourceTypeAvailable(
+            UIImagePickerControllerSourceType.camera)
+        {
+            let imagePicker = UIImagePickerController()
+            imagePicker.delegate = self
+            imagePicker.sourceType = UIImagePickerControllerSourceType.photoLibrary
+            imagePicker.allowsEditing = true
+            self.present(imagePicker, animated: true, completion: nil)
+        }
+    }
+    
+    func saveImgToLibrary() {
+        if (imageView.image != nil) {
+            let imagedata = UIImagePNGRepresentation(imageView.image!)
+            let compressImage = UIImage(data: imagedata!)
+            UIImageWriteToSavedPhotosAlbum(compressImage!, nil, nil, nil)
+            
+            let alert = UIAlertController(title: "Have Saved Image", message: nil, preferredStyle: UIAlertControllerStyle.alert)
+            
+            let actionCancel = UIAlertAction(title: "Confirm", style: UIAlertActionStyle.default, handler: nil)
+            self.present(alert, animated: true, completion: nil)
+            alert.addAction(actionCancel)
+        }
+    }
 }
