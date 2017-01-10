59ed9d95a21d988307ecc38210ba48515d1eae19
Lenny Duan <dhy939694@gmail.com>
2017-01-10 19:18:06 +0000

    db save

---
+++ IOS/TODO/db/People.swift
@@ -0,0 +1,17 @@
+//
+//  People.swift
+//  ToDo
+//
+//  Created by 段鸿易 on 12/6/16.
+//  Copyright © 2016 Lenny. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+
+class People: NSManagedObject {
+
+// Insert code here to add functionality to your managed object subclass
+
+}
